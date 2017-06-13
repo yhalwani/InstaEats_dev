cc1f0f9bbce8b52aeb4edb2a086fad313c8855ae
Youssef <youssef.halwani@gmail.com>
2017-06-13 17:25:09 -0400

    Tweaked bundle structure

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -97,24 +97,37 @@ export class MenuPage {
 export class ModalContentPage {
 
   menuGroup: Array<{
-    menuGroupName:              string,
-    menu:          Array<{name: string, description: string, price: number}>
+    menuGroupName:         string,
+    menu:                  Array<{
+      name: string, description: string, price: number
+    }>
   }>;
 
   bundles: Array<{
     bundleName:            string,
     bundleDescription:     string,
-    bundleElem:        Array<{name: string, description: string, price: number, checked: boolean, discount: number}>
+    bundleElem:            Array<{
+      menuGroupName:       string,
+      menu:                Array<{
+        name: string, description: string, price: number, checked: boolean, discount: number
+      }>
+    }>
   }>;
 
-  bundleItems:          Array<{name: string, description: string, price: number, checked: boolean, discount: number}>;
+  bundleItem: {
+    bundleName:            string,
+    bundleDescription:     string,
+    bundleElem:            Array<{
+      menuGroupName:       string,
+      menu:                Array<{
+        name: string, description: string, price: number, checked: boolean, discount: number
+      }>
+    }>
+  };
 
   bundleName:                   string;
   bundleDescription:            string;
   discount:                     number;
-
-  checked: Array< boolean >;
-
 
   constructor(
     public params: NavParams,
@@ -122,25 +135,56 @@ export class ModalContentPage {
     public storage: Storage,
     public events: Events
   ) {
+
+    // Menu is assigned from the input parameter data
     this.menuGroup = this.params.data;
+
+    // Fetch bundles from storage and assign to local variable
     this.storage.get('bundles').then((list) => {
       this.bundles = list;
     });
 
-    this.bundleItems = [];
+    //
+    this.bundleItem = {
+      bundleName:            "",
+      bundleDescription:     "",
+      bundleElem: []
+    };
+
+    // var item = {
+    //   name: "",
+    //   description: "",
+    //   price: 0,
+    //   checked: false,
+    //   discount: 0
+    // };
 
     for (var i = 0; i < this.menuGroup.length; i++ ){
+
+      var menuItem = {
+        menuGroupName:  "",
+        menu: []
+      }
+
+      menuItem.menuGroupName =  this.menuGroup[i].menuGroupName;
+      this.bundleItem.bundleElem.push(menuItem);
+
       for (var j = 0; j < this.menuGroup[i].menu.length; j++ ){
-        var bundleItem = {
+
+       var item = {
           name: this.menuGroup[i].menu[j].name,
           description: this.menuGroup[i].menu[j].description,
           price: this.menuGroup[i].menu[j].price,
           checked: false,
           discount: 0
         };
-        this.bundleItems.push(bundleItem);
+
+        this.bundleItem.bundleElem[i].menu.push(item);
+
       };
     };
+
+    console.log(this.bundleItem);
 
   };
 
@@ -150,27 +194,23 @@ export class ModalContentPage {
 
   addToBundle(menuItem, total, group, index, length, checked){
     if (checked == true){
-      console.log(checked.checked + "  " + total + "  " + group + "  " + index + " " + length);
+      // console.log(checked.checked + "  " + total + "  " + group + "  " + index + " " + length);
       // var item = {name: menuItem.name, description: menuItem.description, price: menuItem.price, discount: this.discount};
       // this.bundleItem.push(item);
     } else {
-      console.log(checked.checked + "  " + total + "  " + group + "  " + index + " " + length);
+      // console.log(checked.checked + "  " + total + "  " + group + "  " + index + " " + length);
       // this.bundleItem.splice(index,1);
     }
 
   }
 
   saveBundle() {
-    var bundleGroupElem = {bundleName: this.bundleName, bundleDescription: this.bundleDescription, bundleElem: []};
+    // var bundleGroupElem = {bundleName: this.bundleName, bundleDescription: this.bundleDescription, bundleElem: this.bundleItem};
     this.storage.get('bundles').then((list) => {
       this.bundles = list;
-      this.bundles.push(bundleGroupElem);
+      // this.bundles.push(bundleGroupElem);
       this.storage.set('bundles', this.bundles);
-
-      console.log("Bundle Created");
-      console.log(this.bundles);
       this.events.publish('bundle:created', this.bundles);
-
     });
 
     this.dismiss();
