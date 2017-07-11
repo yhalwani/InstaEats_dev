d802ae5839a1e48116fdc696b8d154d58b80178a
Youssef <youssef.halwani@gmail.com>
2017-07-11 14:30:02 -0400

    CSS changes and update to slogan instead of blurb

--- src/pages/discounts/discounts.ts
+++ src/pages/discounts/discounts.ts
@@ -34,34 +34,48 @@ export class DiscountsPage {
     var restaurantId = firebase.auth().currentUser.uid;
     var bundleNode = firebase.database().ref("/Bundles/" + restaurantId);
 
+    // Entering the restaurants bundles node in fb
     bundleNode.once('value', (snapshot) => {
+      //
       console.log("In bundleNode");
+      //
 
+      // For each bundle the rest has loop
       snapshot.forEach( (childSnapshot) => {
+        //
         console.log("  In forEach loop of " + childSnapshot.key);
+        //
+        // Tmp dummy bundle
         var bundle = {
           bundleName: childSnapshot.key,
           bundleDescription:"",
           live: false,
           bundleElem: []
         }
+
+        // For each node in bundle (bunlde, description, or live) loop
         childSnapshot.forEach((childSnapshot) => {
-                  console.log("    In forEach loop of " + childSnapshot.key);
+          //
+          console.log("    In forEach loop of " + childSnapshot.key);
+          //
           if (childSnapshot.key == "description"){
             bundle.bundleDescription = childSnapshot.val();
           } else if (childSnapshot.key == "live") {
             bundle.live = childSnapshot.val();
           } else {
+            //
             childSnapshot.forEach((childSnapshot) => {
-                      console.log("      In forEach loop of " + childSnapshot.key);
+              //
+              console.log("      In forEach loop of " + childSnapshot.key);
+              //
               var bundleE = {menuGroupName:"", menu: []};
               childSnapshot.forEach((childSnapshot) => {
-                        console.log("        In forEach loop of " + childSnapshot.key);
+                console.log("        In forEach loop of " + childSnapshot.key);
                 if(childSnapshot.key == "menuGroupName"){
                   bundleE.menuGroupName = childSnapshot.val();
                 } else {
                   childSnapshot.forEach((childSnapshot) => {
-                            console.log("          In forEach loop of " + childSnapshot.key);
+                    console.log("          In forEach loop of " + childSnapshot.key);
                     var tmp = childSnapshot.val();
                     var menu = {
                       name:         tmp.name,
@@ -89,6 +103,7 @@ export class DiscountsPage {
     });
 
     this.events.subscribe('bundle:created', (bundle) => {
+      console.log("In the subscription");
       this.bundles = bundle;
     });
 
